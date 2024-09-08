cb55f6e63f05e9830690cd1b93797e65e888c032
Tharusha <156021977+CodeMaestro2001@users.noreply.github.com>
2024-09-08 11:25:04 +0530

    completed ui

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import search_icon from '../assets/search.png'
+import clear_icon from '../assets/clear.png'
+import humidity_icon from '../assets/humidity.png'
+import wind_icon from '../assets/wind.png'
+
 import './Weather.css'
 
 
@@ -10,6 +14,28 @@ const Weather = () => {
       <div className='search-bar'>
         <input type='text' placeholder='Search'/>
         <img src={search_icon} alt='search image'/>      
+      </div>
+      <img src={clear_icon} className='clear_icon' />
+      <p className='temperature'>20*C</p>
+      <p className='location'>Malabe</p>
+
+      <div className="weather-data">
+        <div className="col">
+        <img src={wind_icon} />
+          <div>
+            <p>45%</p>
+            <span>Wind</span>
+          </div>
+        </div>
+
+        <div className="col">
+        <img src={humidity_icon} />
+
+          <div>
+            <p>32%</p>
+            <span>Humidity</span>
+          </div>
+        </div>
       </div>
     </div>
   )
